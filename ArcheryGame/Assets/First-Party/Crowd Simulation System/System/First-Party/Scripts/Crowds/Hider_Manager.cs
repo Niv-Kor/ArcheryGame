b8bd3cbfb5fcbe2aa3b033f71259be7be29d82bc
Niv Kor <nivkor23@gmail.com>
2019-09-11 01:15:46 +0300

    Added audience

---
+++ ArcheryGame/Assets/First-Party/Crowd Simulation System/System/First-Party/Scripts/Crowds/Hider_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider_Manager : MonoBehaviour
+
+{
+
+    public GameObject Sign_1;
+
+    void Update()
+
+    {
+
+        if (GetComponent<Animation>().IsPlaying("wave_1"))
+
+        {
+
+            Sign_1.SetActive(true);
+
+        }
+
+        else
+
+        {
+
+            Sign_1.SetActive(false);
+
+        }
+
+    }
+
+}
